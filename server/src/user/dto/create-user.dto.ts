3a88354149142168b4bb23de30305a428e140a08
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-12 14:57:40 +0700

    :alien: Update User-dto

--- server/src/user/dto/create-user.dto.ts
+++ server/src/user/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsOptional()
+  email?: string;
+
+  @IsString()
+  @IsOptional()
+  username?: string;
+
+  @IsString()
+  @IsOptional()
+  password?: string;
+}
