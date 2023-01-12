517f69d213aa16f2eb63ff527bc722701852e889
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-12 14:53:33 +0700

    :alien: Add User Resource

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule, ProductModule],
+  imports: [PrismaModule, ProductModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
